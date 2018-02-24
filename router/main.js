3a9f500bf122c5f1651c22386e674d14292e92ff
Daniel Carl Jones <danny@danielcarl.info>
2018-02-24 17:34:15 +0000

    Refactor main router
    This change makes usage of the main router a little nicer. We create a router object, define all the routes, and then we can export and use this router on any path as 'middleware'.

--- router/main.js
+++ router/main.js
@@ -1,3 +1,9 @@
+// This file was slightly rewritten to instead use the Router object in express. No longer need to pass around 'app'.
+// Routes are instead registered with a 'router' object rather than 'app', which is then exported and can be used in the app in server.js.
+
+const express = require('express')
+const router = express.Router()
+
 const Loki = require('lokijs') // http://lokijs.org/
 const db = new Loki('db.json', {
   autosave: true,
@@ -29,46 +35,46 @@ function databaseInit () {
   }
 }
 
-module.exports = function (app) {
-  app.get('/', function (req, res) {
-    res.render('index', {
-      gifs: dayGifs
-    })
+router.get('/', function (req, res) {
+  res.render('index', {
+    gifs: dayGifs
   })
-  
-  app.get('/votes', function (req, res) {
-    // the weird => thing is called "arrow syntax", 
-    //shorthand for funcs (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions)
-    const votes = dayVotes.data.map(g => g.count) 
-  
-    res.json({
-      votes: votes
-    })
+})
+
+router.get('/votes', function (req, res) {
+  // the weird => thing is called "arrow syntax", 
+  //shorthand for funcs (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions)
+  const votes = dayVotes.data.map(g => g.count) 
+
+  res.json({
+    votes: votes
   })
-  
-  app.post('/upvote/:gifID', function (req, res) {
-    const gif = dayVotes.get(req.params.gifID)
-    
-    dayVotes.update({
-      $loki: gif.$loki,
-      meta: gif.meta,
-      url: gif.url,
-      count: gif.count + 1
-    })
-    
-    res.send(true)
+})
+
+router.post('/upvote/:gifID', function (req, res) {
+  const gif = dayVotes.get(req.params.gifID)
+
+  dayVotes.update({
+    $loki: gif.$loki,
+    meta: gif.meta,
+    url: gif.url,
+    count: gif.count + 1
   })
-  
-  app.post('/downvote/:gifID', function (req, res) {
-    const gif = dayVotes.get(req.params.gifID)
-    
-    dayVotes.update({
-      $loki: gif.$loki,
-      meta: gif.meta,
-      url: gif.url,
-      count: gif.count - 1
-    })
-    
-    res.send(true)
+
+  res.send(true)
+})
+
+router.post('/downvote/:gifID', function (req, res) {
+  const gif = dayVotes.get(req.params.gifID)
+
+  dayVotes.update({
+    $loki: gif.$loki,
+    meta: gif.meta,
+    url: gif.url,
+    count: gif.count - 1
   })
-}
+
+  res.send(true)
+})
+
+module.exports = router; // Now, we just export a router object rather than a func making a router on the app.
--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const express = require('express') // https://expressjs.com/
 const app = express()
 const port = 3000
 
-require('./router/main')(app) // Get the router we created in router/main.js
+// These two lines modified to use a nicer syntax rather than passing our 'app' object around. See router/main.js for more info.
+const mainRouter = require('./router/main')
+app.use(mainRouter)
 
 app.use(express.static('public')) // Enable us to find 'static' files in public under our.url/
 app.set('view engine', 'pug')
